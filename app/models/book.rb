17d28a0b8a9485dbf72ac0c16ebcd0ce03a121e8
nieves-mon <cunanan.nm@gmail.com>
2022-10-13 21:05:10 +0800

    Add validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+    validates :title, presence: true
+    validates :author, presence: true
+    validates :description, presence: true
+    validates :status, presence:true
 end
