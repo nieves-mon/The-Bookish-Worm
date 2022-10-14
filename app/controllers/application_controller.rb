de7fffd9117d7b955a517a5f09e9ead05ba3bbc3
nieves-mon <cunanan.nm@gmail.com>
2022-10-14 18:29:32 +0800

    Add helper function 'set_book'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :set_book, only: [:show, :edit, :update, :destroy]
+
     def index
         @books = Book.all
     end
 
     def show
-        @book = Book.find(params[:id])
     end
 
     def new
@@ -21,7 +22,19 @@ class ApplicationController < ActionController::Base
         end
     end
 
+    def edit
+
+    end
+
+    def update
+
+    end
+
     private
+
+        def set_book
+            @book = Book.find(params[:id])
+        end
 
         def book_params
             params.require(:book).permit(:title, :author, :description, :status, :rating, :review)
