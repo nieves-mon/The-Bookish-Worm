502ef0ed8b2681b730e776ed04a856d09eeb504e
nieves-mon <cunanan.nm@gmail.com>
2022-10-15 00:43:18 +0800

    Add basic layout to form

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,5 @@ class Book < ApplicationRecord
     validates :title, presence: true
     validates :author, presence: true
     validates :status, presence:true
-    validates :rating, numericality: {less_than_or_equal_to: 5, more_than_or_equal_to: 0}, allow_nil: true
+    validates :rating, numericality: {less_than_or_equal_to: 5, more_than_or_equal_to: 1}, allow_nil: true
 end
