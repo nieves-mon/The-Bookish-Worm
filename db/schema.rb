7837ab32d1d41d0ac2615aba72988f2710cd6741
nieves-mon <cunanan.nm@gmail.com>
2022-10-14 19:04:11 +0800

    Change from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2022_10_13_115118) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.string "status"
-    t.text "description"
     t.integer "rating"
-    t.text "review"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
