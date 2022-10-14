c04c8ad5d9187b1e0710865a06cec6606c874518
nieves-mon <cunanan.nm@gmail.com>
2022-10-14 19:12:34 +0800

    Remove description validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,6 @@
 class Book < ApplicationRecord
     validates :title, presence: true
     validates :author, presence: true
-    validates :description, presence: true
     validates :status, presence:true
     validates :rating, numericality: {less_than_or_equal_to: 5, more_than_or_equal_to: 0}, allow_nil: true
 end
