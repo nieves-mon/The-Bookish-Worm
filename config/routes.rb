02e912813e59300c8d70390228a223860f5f508a
nieves-mon <cunanan.nm@gmail.com>
2022-10-14 17:25:22 +0800

    Add all routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   root "books#index"
 
   get "/books", to: "books#index"
+  post "/books", to: "books#create"
+  get "/books/new", to: "books#new", as: "new_book"
+  get "/books/:id/edit", to: "books#edit", as: "edit_book"
   get "/books/:id", to: "books#show", as: "book"
+  put "/books/:id", to: "books#update"
+  delete "/books/:id", to: "books#destroy"
 end
