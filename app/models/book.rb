220d837875df61c451e939557854dc9ffbb993a8
nieves-mon <cunanan.nm@gmail.com>
2022-10-14 18:24:46 +0800

    Show no rating/review

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,5 @@ class Book < ApplicationRecord
     validates :author, presence: true
     validates :description, presence: true
     validates :status, presence:true
-    validates :rating, numericality: {less_than_or_equal_to: 5, more_than_or_equal_to: 0}
+    validates :rating, numericality: {less_than_or_equal_to: 5, more_than_or_equal_to: 0}, allow_nil: true
 end
